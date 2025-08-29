e2e9902a9a9c7ea947f5207a8c8784a9bcbfa8c0
rizkyramadika <rizky@liquidx.studio>
2025-08-29 07:50:03 +0700

    uninstall aos

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "aos/dist/aos.css";
 import App from "@/pages/user-directory";
 
 createRoot(document.getElementById("root")!).render(
